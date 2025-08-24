8d0a56125390a7a1b7da9b343bd986facff97e79
lahit1 <naifcanbasci@gmail.com>
2025-08-24 19:07:45 +0300

    Let Radix::N handle it!

--- lib/unibus.cxx
+++ lib/unibus.cxx
@@ -14,16 +14,7 @@ namespace unibus {
 		return p;
 	}
 
-	void T::use(std::initializer_list<char> &header, void (*worker_func)(Req& req, Res& res)) {
-		std::unordered_map<char, std::shared_ptr<Radix::N>> *par = &rad_root.targets;
-		std::shared_ptr<Radix::N> last_node;
-		for(char c: header) {
-			last_node = (*par)[c];
-			if(last_node == nullptr)
-				last_node = (*par)[c] = std::make_shared<Radix::N>();
-			par = &last_node->targets;
-		}
-		last_node->header = header;
-		last_node->worker_func = worker_func;
+	void T::use(const std::initializer_list<char> &header, void (*worker_func)(Req& req, Res& res)) {
+		rad_root.use(header, worker_func);
 	}
 }
